c474e00942bef2b521945785071cbc0fe3b7ca10
Bobby Koller <kolbobby@gmail.com>
2013-03-20 19:42:06 -0400

    Working on Twitter post

--- application/controllers/twitter.php
+++ application/controllers/twitter.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Twitter extends CI_Controller {
-	public function post($msg) {
-		return $msg;
+	public function post() {
+		return $_POST['content'];
 	}
 }
