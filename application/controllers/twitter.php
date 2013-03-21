cba36badb0242604108cb1a2d51d997a73508d7f
Bobby <kolbobby@gmail.com>
2013-03-20 23:19:30 -0400

    Working on Twitter API

---
+++ application/controllers/twitter.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Twitter extends CI_Controller {
+	private $consumerKey = 'waCJgHUYryh7MKx1duFEqA';
+	private $consumerSecret = 'wZ7ZyCtxkG41S7rqLrBqmYfj6vFuLnSslAQlt91bovM';
+	private $OAuthToken = '726047942-1xIWuuR4i57yTvYsihOzzCEvL1VmHZVBj27GRekr';
+	private $OAuthSecret = '3kqCWrdRkma8BbsVu0MfKTaxMJimUbah78udPOY57g';
+	
+	private $connection;
+	public function __construct() {
+		parent::__construct();
+		
+		$this->connection = $this->twitteroauch->create($consumerKey, $consumerSecret, $OAuthToken, $OAuthSecret);
+	}
+	
+	public function post() {
+		echo $this->connection;
+	}
+}
