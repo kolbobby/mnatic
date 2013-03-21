04b571328fad3669b1563f24491579d69dfbc69c
Bobby <kolbobby@gmail.com>
2013-03-20 22:49:07 -0400

    Working on Twitter API

--- application/config/twitter.php
+++ application/config/twitter.php
@@ -2,5 +2,5 @@
 
 $config['twitter_consumer_token']		= 'waCJgHUYryh7MKx1duFEqA';
 $config['twitter_consumer_secret']		= 'wZ7ZyCtxkG41S7rqLrBqmYfj6vFuLnSslAQlt91bovM';
-$config['twitter_access_token']			= '726047942-1xIWuuR4i57yTvYsihOzzCEvL1VmHZVBj27GRekr';
-$config['twitter_access_secret']		= '3kqCWrdRkma8BbsVu0MfKTaxMJimUbah78udPOY57g';
+$config['oauth_token']			= '726047942-1xIWuuR4i57yTvYsihOzzCEvL1VmHZVBj27GRekr';
+$config['oauth_token_secret']		= '3kqCWrdRkma8BbsVu0MfKTaxMJimUbah78udPOY57g';
