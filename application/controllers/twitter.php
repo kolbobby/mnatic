49d8b5fb1c07d7ce1c38268d9e8b9102c8997289
Bobby <kolbobby@gmail.com>
2013-03-20 20:08:16 -0400

    Working on Twitter post

--- application/controllers/twitter.php
+++ application/controllers/twitter.php
@@ -1,7 +1,24 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Twitter extends CI_Controller {
+	public function Twitter() {
+		parent::__construct();
+		
+		// INSERT KEYS
+		$consumer_key = "waCJgHUYryh7MKx1duFEqA";
+		$consumer_secret = "wZ7ZyCtxkG41S7rqLrBqmYfj6vFuLnSslAQlt91bovM";
+		$access_key = "726047942-1xIWuuR4i57yTvYsihOzzCEvL1VmHZVBj27GRekr";
+		$access_secret = "3kqCWrdRkma8BbsVu0MfKTaxMJimUbah78udPOY57g";
+		
+		// IMPORT TWITTER OAUTH
+		$this->load->library('TwitterOAuth');
+		
+		// CREATE NEW TWEET INSTANCE
+		$this->$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $OAuthToken, $OAuthSecret);
+	}
+
 	public function post() {
-		echo $_POST['content'];
+		$post = $_POST['content'];
+		$this->$tweet->post('statuses/update', array('status' => "$post"));
 	}
 }
