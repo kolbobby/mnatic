71e7226130379e500974b34186668e44d3adc286
Bobby <kolbobby@gmail.com>
2013-03-21 00:44:05 -0400

    Adding Twitter API

--- application/config/twitter.php
+++ application/config/twitter.php
@@ -1,9 +1,9 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['twitter_consumer_token']		= 'your_consumer_token';
-$config['twitter_consumer_secret']		= 'your_consumer_token_secret';
-$config['twitter_access_token']			= 'your_access_token';
-$config['twitter_access_secret']		= 'your_access_token_secret';
+$config['twitter_consumer_token']		= 'waCJgHUYryh7MKx1duFEqA';
+$config['twitter_consumer_secret']		= 'wZ7ZyCtxkG41S7rqLrBqmYfj6vFuLnSslAQlt91bovM';
+$config['twitter_access_token']			= '726047942-1xIWuuR4i57yTvYsihOzzCEvL1VmHZVBj27GRekr';
+$config['twitter_access_secret']		= '3kqCWrdRkma8BbsVu0MfKTaxMJimUbah78udPOY57g';
 
 /* End of file twitter.php */
 /* Location: ./application/config/twitter.php */
