62765c378d50175b9cda82a72409640270e27dc9
Bobby <kolbobby@gmail.com>
2013-03-20 23:45:54 -0400

    Working on Twitter API

--- application/controllers/twitter.php
+++ application/controllers/twitter.php
@@ -1,17 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Twitter extends CI_Controller {
-	/*private $consumerKey = 'waCJgHUYryh7MKx1duFEqA';
-	private $consumerSecret = 'wZ7ZyCtxkG41S7rqLrBqmYfj6vFuLnSslAQlt91bovM';
-	private $OAuthToken = '726047942-1xIWuuR4i57yTvYsihOzzCEvL1VmHZVBj27GRekr';
-	private $OAuthSecret = '3kqCWrdRkma8BbsVu0MfKTaxMJimUbah78udPOY57g';
-	
 	private $connection;
 	public function __construct() {
 		parent::__construct();
 		
-		$this->connection = $this->twitteroauth->create($this->$consumerKey, $this->$consumerSecret, $this->$OAuthToken, $this->$OAuthSecret);
-	}*/
+		$consumerKey = 'waCJgHUYryh7MKx1duFEqA';
+		$consumerSecret = 'wZ7ZyCtxkG41S7rqLrBqmYfj6vFuLnSslAQlt91bovM';
+		$OAuthToken = '726047942-1xIWuuR4i57yTvYsihOzzCEvL1VmHZVBj27GRekr';
+		$OAuthSecret = '3kqCWrdRkma8BbsVu0MfKTaxMJimUbah78udPOY57g';
+		$this->connection = $this->twitteroauth->create($consumerKey, $consumerSecret, $OAuthToken, $OAuthSecret);
+	}
 	
 	public function post() {
 		echo $_POST['content'];
