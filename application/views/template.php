4ee66a95c1298658ab9176cecb6f23750f7800ea
Bobby Koller <kolbobby@aol.com>
2013-03-29 15:48:20 -0400

    Working on navbar

--- application/views/template.php
+++ application/views/template.php
@@ -8,10 +8,6 @@
 		
 		<link rel="StyleSheet" href="<?= base_url(); ?>assets/css/bootstrap.css" />
 		<style type="text/css">
-			body {
-				padding-top:40px;
-			}
-
 			img {
 				width:100%;
 				height:300px;
