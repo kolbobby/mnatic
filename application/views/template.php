a8cafe04a2cc6f25dfea7cafe44f86c4d2a15517
Bobby Koller <kolbobby@aol.com>
2013-03-29 15:56:11 -0400

    Working on navbar

--- application/views/template.php
+++ application/views/template.php
@@ -38,11 +38,20 @@
 		<div class="navbar navbar-inverse navbar-fixed-top">
 			<div class="navbar-inner">
 				<div class="center-margin">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+
 					<a class="brand" href="/">mNatic</a>
-					<ul class="nav pull-right">
-						<li class="active"><a href="/">Home</a></li>
-						<li><a href="#">Sign In</a></li>
-					</ul>
+
+					<div class="nav-collapse collapse">
+						<ul class="nav pull-right">
+							<li class="active"><a href="/">Home</a></li>
+							<li><a href="#">Sign In</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
