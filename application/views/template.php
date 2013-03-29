fb52d2614cf64cabc7b44ea9ee4b344eb4e2f20a
Bobby Koller <kolbobby@aol.com>
2013-03-29 16:12:14 -0400

    Working on navbar

--- application/views/template.php
+++ application/views/template.php
@@ -12,15 +12,6 @@
 				width:100%;
 				height:300px;
 			}
-
-			.navbar {
-				position:absolute;
-			}
-			
-			.center-margin {
-				width:90%;
-				margin:0 5% 0 5%;
-			}
 		</style>
 		<link rel="StyleSheet" href="<?= base_url(); ?>assets/css/bootstrap-responsive.css" />
 
@@ -35,24 +26,28 @@
 	
 	<body>
 		<?php include_once('includes/carousel.html'); ?>
-		<div class="navbar navbar-inverse center-margin">
-			<div class="navbar-inner">
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
+		<div class="navbar-wrapper">
+			<div class="container">
+				<div class="navbar navbar-inverse">
+					<div class="navbar-inner">
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
 
-				<a class="brand" href="/">mNatic</a>
+						<a class="brand" href="/">mNatic</a>
 
-				<div class="nav-collapse collapse">
-					<ul class="nav pull-right">
-						<li class="active"><a href="/">Home</a></li>
-						<li><a href="#">Sign In</a></li>
-					</ul>
+						<div class="nav-collapse collapse">
+							<ul class="nav pull-right">
+								<li class="active"><a href="/">Home</a></li>
+								<li><a href="#">Sign In</a></li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="center-margin"><?php $this->load->view($content); ?></div>
+		<div class="container"><?php $this->load->view($content); ?></div>
 	</body>
 </html>
